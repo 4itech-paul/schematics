24c5daaccb8f1ab40f4e78d1fab4ec4041bb97dd
4itech-paul <paul@4idps.com>
2024-01-01 00:38:24 +0800

    refactor(main): switch to main-dao-node-page for generic type handling
    - replace DaoNodePage with MainDaoNodePage in page type definitions
    - update associated imports and nodes field typing in entity and test files
    - ensure consistency and leverage common type enhancements across the platform

--- src/lib/main/files/ts/type/__name@singular@page@ent__.ts
+++ src/lib/main/files/ts/type/__name@singular@page@ent__.ts
@@ -1,12 +1,12 @@
-<% if (type === 'graphql-code-first') { %>import { DaoNodePage } from '@app/graphql-type/type/dao-node-page.type';
 import { Field, ObjectType } from '@nestjs/graphql';
+import { MainDaoNodePage } from 'apps/main/src/common/main-dao-node-page.type';
 
-import { <%= singular(classify(name)) %>Type } from './<%= singular(name) %>.type';
+import { <%= singular(classify(name)) %> } from '../<%= singular(lowercased(name)) %>.entity';
 
 @ObjectType('<%= singular(classify(name)) %>Page', {
-  implements: [DaoNodePage],
+  implements: [MainDaoNodePage],
 })
-export class <%= singular(classify(name)) %>PageType implements DaoNodePage<<%= singular(classify(name)) %>Type> {
-  @Field(() => [<%= singular(classify(name)) %>Type], { description: 'Nodes in this page' })
-  nodes!: <%= singular(classify(name)) %>Type[];
-}<% } else { %>export class <%= singular(classify(name)) %>Type {}<% } %>
+export class <%= singular(classify(name)) %>PageType implements MainDaoNodePage<<%= singular(classify(name)) %>> {
+  @Field(() => [<%= singular(classify(name)) %>], { description: 'Nodes in this page' })
+  nodes!: <%= singular(classify(name)) %>[];
+}
