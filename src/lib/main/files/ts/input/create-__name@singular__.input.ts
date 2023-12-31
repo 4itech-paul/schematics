8346ddfabb2dbb0c80358bbe77a64dce77cd58b8
4itech-paul <paul@4idps.com>
2024-01-01 00:50:00 +0800

    refactor(input): extend create-input from omit-meta-entity-type
    - Removed explicit id field in favor of inheritance from meta entity type
    - Simplified import statements for input types in code generation tests

--- src/lib/main/files/ts/input/create-__name@singular__.input.ts
+++ src/lib/main/files/ts/input/create-__name@singular__.input.ts
@@ -1,7 +1,7 @@
-<% if (type === 'graphql-code-first') { %>import { Field, ID, InputType } from '@nestjs/graphql';
+import { OmitMetaEntityType } from '@app/graphql-type/omit-meta-entity-type';
+import { InputType } from '@nestjs/graphql';
+
+import { <%= singular(classify(name)) %> } from '../<%= singular(lowercased(name)) %>.entity';
 
 @InputType()
-export class Create<%= singular(classify(name)) %>Input {
-  @Field(() => ID, { description: 'Example field' })
-  id!: string;
-}<% } else { %>export class Create<%= singular(classify(name)) %>Input {}<% } %>
+export class Create<%= singular(classify(name)) %>Input extends OmitMetaEntityType(<%= singular(classify(name)) %>, [] as const) {}
