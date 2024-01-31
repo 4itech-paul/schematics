cbefccfe9260955f34b483b3dbf577699f1ed79e
4itech-paul <paul@4idps.com>
2024-01-31 10:37:39 +0800

    feat(main): add loaders and with-users resolver to user module tests
    - Introduce tests for new users-by-users-id.loader and
    with-users.resolver modules
    - Update UserResolver test suite to reflect added queries and mutations
    - Ensure all relevant user entity operations are covered in the test
    scenarios

---
+++ src/lib/main/files/ts/with-__name__.resolver.ts
@@ -0,0 +1,21 @@
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { Maybe } from 'graphql/jsutils/Maybe';
+
+import { <%= classify(singular(name)) %>By<%= classify(singular(name)) %>IdLoader } from './<%= singular(name) %>-by-<%= singular(name) %>-id.loader';
+import { <%= classify(singular(name)) %> } from './<%= singular(name) %>.entity';
+import { With<%= classify(singular(name)) %> } from './type/with-<%= singular(name) %>.type';
+
+@Resolver(() => With<%= classify(singular(name)) %>)
+export class With<%= classify(singular(name)) %>Resolver {
+  constructor(
+    private readonly loader: <%= classify(singular(name)) %>By<%= classify(singular(name)) %>IdLoader,
+  ) {}
+
+  @ResolveField(() => <%= classify(singular(name)) %>, { nullable: true })
+  async <%= lowercased(singular(name)) %>(
+    @Parent() { <%= lowercased(singular(name)) %>Id, <%= lowercased(singular(name)) %> }: With<%= classify(singular(name)) %>,
+  ): Promise<Maybe<<%= classify(singular(name)) %>>> {
+    if (<%= lowercased(singular(name)) %> || !<%= lowercased(singular(name)) %>Id) return;
+    return this.loader.load(<%= lowercased(singular(name)) %>Id);
+  }
+}
