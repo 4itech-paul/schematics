6497e36530a4fbad39df031acb9833a710a7f666
4itech-paul <paul@4idps.com>
2024-02-21 11:33:22 +0800

    feat(input): streamline entity input type generation
    - replace specific base input types with generalized ToCreateInputType,
      ToOrderInputType, and ToWhereInputType
    - remove unused example fields and methods
    - update test cases to reflect new base input type usage
    - improve maintainability of input type definitions across modules

--- src/lib/main/files/ts/input/__name@singular__-where.input.ts
+++ src/lib/main/files/ts/input/__name@singular__-where.input.ts
@@ -1,13 +1,13 @@
-import { PartialAndOmitType } from '@app/graphql-type/partial-and-omit-type';
+import { ToWhereInputType } from '@app/graphql-type/to-where-input-type';
 import { InputType } from '@nestjs/graphql';
 import { Nullable } from 'apps/main/src/common/base.service';
 import { FindOptionsWhere } from 'typeorm';
 
-import { <%= singular(classify(name)) %> } from '../<%= singular(name) %>.entity';
+import { <%= classify(singular(name)) %> } from '../<%= singular(name) %>.entity';
 
 @InputType()
-export class <%= singular(classify(name)) %>WhereInput extends PartialAndOmitType(<%= singular(classify(name)) %>, []) {
-  toFindOptionsWhere(): Nullable<FindOptionsWhere<<%= singular(classify(name)) %>>> | undefined {
+export class <%= classify(singular(name)) %>WhereInput extends ToWhereInputType(<%= classify(singular(name)) %>) {
+  toFindOptionsWhere(): Nullable<FindOptionsWhere<<%= classify(singular(name)) %>>> | undefined {
     const { ...where } = this;
 
     return { ...where };
--- src/lib/main/files/ts/input/create-__name@singular__.input.ts
+++ src/lib/main/files/ts/input/create-__name@singular__.input.ts
@@ -1,7 +1,7 @@
-import { OmitMetaEntityType } from '@app/graphql-type/omit-meta-entity-type';
+import { ToCreateInputType } from '@app/graphql-type/to-create-input-type';
 import { InputType } from '@nestjs/graphql';
 
 import { <%= singular(classify(name)) %> } from '../<%= singular(name) %>.entity';
 
 @InputType()
-export class Create<%= singular(classify(name)) %>Input extends OmitMetaEntityType(<%= singular(classify(name)) %>, [] as const) {}
+export class Create<%= singular(classify(name)) %>Input extends ToCreateInputType(<%= singular(classify(name)) %>) {}
