1503e74ba8ebef35ddf2262577c5fbb0f2ea4c6f
4itech-paul <paul@4idps.com>
2024-03-14 11:25:16 +0800

    refactor(service): standardize entity and dto naming conventions
    - replace generic `Domain1` references with dynamic entity name templates
    - ensure consistency in DTO, type, and repository variable names
    - improve code maintainability and readability by aligning naming schemes

--- src/lib/main/files/ts/__name__.service.ts
+++ src/lib/main/files/ts/__name__.service.ts
@@ -6,30 +6,30 @@ import { EntityManager, Repository } from 'typeorm';
 
 import { ServiceMetadata } from '../common/service-metadata.interface';
 import { User } from '../user/user.entity';
-import { Domain1PageArgs } from './args/<%= singular(name) %>-page.args';
+import { <%= classify(singular(name)) %>PageArgs } from './args/<%= singular(name) %>-page.args';
 import { <%= classify(singular(name)) %> } from './<%= singular(name) %>.entity';
-import { CreateDomain1Input } from './input/create-<%= singular(name) %>.input';
-import { UpdateDomain1Input } from './input/update-<%= singular(name) %>.input';
-import { CreateDomain1Output } from './output/create-<%= singular(name) %>.output';
-import { RemoveDomain1Output } from './output/remove-<%= singular(name) %>.output';
-import { UpdateDomain1Output } from './output/update-<%= singular(name) %>.output';
-import { Domain1PageType } from './type/<%= singular(name) %>-page.type';
+import { Create<%= classify(singular(name)) %>Input } from './input/create-<%= singular(name) %>.input';
+import { Update<%= classify(singular(name)) %>Input } from './input/update-<%= singular(name) %>.input';
+import { Create<%= classify(singular(name)) %>Output } from './output/create-<%= singular(name) %>.output';
+import { Remove<%= classify(singular(name)) %>Output } from './output/remove-<%= singular(name) %>.output';
+import { Update<%= classify(singular(name)) %>Output } from './output/update-<%= singular(name) %>.output';
+import { <%= classify(singular(name)) %>PageType } from './type/<%= singular(name) %>-page.type';
 
 @Injectable()
 export class <%= classify(singular(name)) %>Service extends BaseService<<%= classify(singular(name)) %>> {
   constructor(
     private readonly manager: EntityManager,
     @InjectRepository(<%= classify(singular(name)) %>)
-    private readonly domain1Repo: Repository<<%= classify(singular(name)) %>>,
+    private readonly <%= lowercased(singular(name)) %>Repo: Repository<<%= classify(singular(name)) %>>,
   ) {
     super(<%= lowercased(singular(name)) %>Repo);
   }
 
   async createOne(
-    input: CreateDomain1Input,
+    input: Create<%= classify(singular(name)) %>Input,
     user: User,
     metadata?: Pick<ServiceMetadata, 'manager'>,
-  ): Promise<CreateDomain1Output> {
+  ): Promise<Create<%= classify(singular(name)) %>Output> {
     const transaction = async (manager: EntityManager) => {
       const <%= lowercased(singular(name)) %> = await this.save(
         {
@@ -49,9 +49,9 @@ export class <%= classify(singular(name)) %>Service extends BaseService<<%= clas
   }
 
   async findByPageArgs(
-    args: Domain1PageArgs,
+    args: <%= classify(singular(name)) %>PageArgs,
     metadata?: Pick<ServiceMetadata, 'manager'>,
-  ): Promise<Domain1PageType> {
+  ): Promise<<%= classify(singular(name)) %>PageType> {
     return this.findNodePage(
       { ...args, where: args.where.toFindOptionsWhere() },
       metadata,
@@ -63,23 +63,23 @@ export class <%= classify(singular(name)) %>Service extends BaseService<<%= clas
     metadata?: Pick<ServiceMetadata, 'manager'>,
   ): Promise<<%= classify(singular(name)) %> | null> {
     if (metadata?.manager) {
-      const domain1Repo = metadata.manager.getRepository(<%= classify(singular(name)) %>);
-      return domain1Repo.findOneBy({ id });
+      const <%= lowercased(singular(name)) %>Repo = metadata.manager.getRepository(<%= classify(singular(name)) %>);
+      return <%= lowercased(singular(name)) %>Repo.findOneBy({ id });
     }
 
-    return this.domain1Repo.findOneBy({ id });
+    return this.<%= lowercased(singular(name)) %>Repo.findOneBy({ id });
   }
 
   async updateOne(
     id: string,
-    input: UpdateDomain1Input,
+    input: Update<%= classify(singular(name)) %>Input,
     user: User,
     metadata?: Pick<ServiceMetadata, 'manager'>,
-  ): Promise<UpdateDomain1Output> {
+  ): Promise<Update<%= classify(singular(name)) %>Output> {
     const transaction = async (manager: EntityManager) => {
-      const domain1Repo = manager.getRepository(<%= classify(singular(name)) %>);
+      const <%= lowercased(singular(name)) %>Repo = manager.getRepository(<%= classify(singular(name)) %>);
 
-      const <%= lowercased(singular(name)) %> = await domain1Repo.preload({
+      const <%= lowercased(singular(name)) %> = await <%= lowercased(singular(name)) %>Repo.preload({
         ...input,
         updatedBy: user.id,
         id,
@@ -88,7 +88,7 @@ export class <%= classify(singular(name)) %>Service extends BaseService<<%= clas
         throw new DaoIdNotFoundError(<%= classify(singular(name)) %>, id);
       }
 
-      await domain1Repo.save(<%= lowercased(singular(name)) %>);
+      await <%= lowercased(singular(name)) %>Repo.save(<%= lowercased(singular(name)) %>);
 
       return {
         <%= lowercased(singular(name)) %>,
@@ -103,7 +103,7 @@ export class <%= classify(singular(name)) %>Service extends BaseService<<%= clas
   async removeOne(
     id: string,
     metadata?: Pick<ServiceMetadata, 'manager'>,
-  ): Promise<RemoveDomain1Output> {
+  ): Promise<Remove<%= classify(singular(name)) %>Output> {
     const transaction = async (manager: EntityManager) => {
       const <%= lowercased(singular(name)) %> = await this.findOneByOrFail({ id }, { manager });
 
