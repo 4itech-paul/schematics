0b4bfcb0e8477eec785bf083f7c5267fa4425630
4itech-paul <paul@4idps.com>
2024-03-31 00:56:37 +0800

    refactor(domain): update class inheritance and argument types
    - modify inheritance strategy for `Domain1` to implement `MetaEntity` as an array
    - include `OmitType` in `UpdateDomain1Input` to refine input class structure
    - introduce `Maybe` type to `Domain1PageArgs` for conditional parameters
    - change `Domain1WhereInput` to be an optional array with nullable attribute

--- src/lib/domain/files/ts/mutation/update-__name@singular__.input.ts
+++ src/lib/domain/files/ts/mutation/update-__name@singular__.input.ts
@@ -1,9 +1,12 @@
-import { Field, ID, InputType, PartialType } from '@nestjs/graphql';
+import { Field, ID, InputType, OmitType, PartialType } from '@nestjs/graphql';
 
 import { Create<%= classify(singular(name)) %>Input } from './create-<%= singular(name) %>.input';
 
 @InputType()
-export class Update<%= classify(singular(name)) %>Input extends PartialType(Create<%= classify(singular(name)) %>Input) {
+export class Update<%= classify(singular(name)) %>Input extends OmitType(
+  PartialType(Create<%= classify(singular(name)) %>Input),
+  [],
+) {
   @Field(() => ID)
   id!: string;
 }
