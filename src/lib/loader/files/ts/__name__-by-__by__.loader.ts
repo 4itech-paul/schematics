cb06a2d1d58bd0e0e6f56697f054048589bb5622
4itech-paul <paul@4idps.com>
2024-06-21 22:50:28 +0800

    docs: update read-me with new loader instructions
    - add loader instructions for search with match case and include files
    - provide specific replacements for `domain-0002s`, `Domain0002s`, and related strings

--- src/lib/loader/files/ts/__name__-by-__by__.loader.ts
+++ src/lib/loader/files/ts/__name__-by-__by__.loader.ts
@@ -3,23 +3,23 @@ import DataLoader from 'dataloader';
 import { Maybe } from 'graphql/jsutils/Maybe';
 import { In } from 'typeorm';
 
-import { Domain0010 } from './domain-0010.entity';
-import { Domain0010Repository } from './domain-0010.repository';
+import { <%= classify(singular(name)) %> } from './<%= singular(name) %>.entity';
+import { <%= classify(singular(name)) %>Repository } from './<%= singular(name) %>.repository';
 
 @Injectable({ scope: Scope.REQUEST })
-export class Domain0010sByDomain0009IdLoader extends DataLoader<
+export class <%= classify(name) %>By<%= classify(by) %>Loader extends DataLoader<
   string,
-  Maybe<Domain0010[]>
+  Maybe<<%= classify(singular(name)) %>[]>
 > {
-  constructor(private readonly repo: Domain0010Repository) {
-    super(async (keys: readonly string[]): Promise<Maybe<Domain0010[]>[]> => {
+  constructor(private readonly repo: <%= classify(singular(name)) %>Repository) {
+    super(async (keys: readonly string[]): Promise<Maybe<<%= classify(singular(name)) %>[]>[]> => {
       const daoArray = await this.repo.find({
         where: {
-          domain0009Id: In(keys),
+          <%= lowercased(by) %>: In(keys),
         },
       });
       return keys.map((key) =>
-        daoArray.filter((dao) => key === dao.domain0009Id),
+        daoArray.filter((dao) => key === dao.<%= lowercased(by) %>),
       );
     });
   }
--- src/lib/loader/loader.factory.test.ts
+++ src/lib/loader/loader.factory.test.ts
@@ -14,22 +14,22 @@ describe('Loader Factory', () => {
   describe('[GraphQL - Code first]', () => {
     it('should generate appropriate files ', async () => {
       const options: DomainOptions = {
-        name: 'loader-0001',
-        by: 'loader-0002',
+        name: 'domain-0002s',
+        by: 'domain-0001-id',
       };
       const tree = await runner.runSchematic('loader', options);
       const files = tree.files;
       expect(files).toEqual([
-        '/loader-0001/loader-0001-by-id.loader.ts',
-        '/loader-0001/loader-0001-by-id.resolver.ts',
-        '/loader-0001/query/loader-0001-by-id.type.ts',
+        '/domain-0002s/domain-0002s-by-domain-0001-id.loader.ts',
+        '/domain-0002s/domain-0002s-by-domain-0001-id.resolver.ts',
+        '/domain-0002s/query/domain-0002s-by-domain-0001-id.type.ts',
       ]);
     });
   });
   describe('[GraphQL - Code first]', () => {
     const options: DomainOptions = {
-      name: 'loader-0001',
-      by: 'loader-0002',
+      name: 'domain-0002s',
+      by: 'domain-0001-id',
     };
 
     let tree: UnitTestTree;
@@ -39,32 +39,30 @@ describe('Loader Factory', () => {
     });
 
     it('should generate "Domain0001ByIdLoader" class', () => {
-      expect(tree.readContent('/loader-0001/loader-0001-by-id.loader.ts'))
+      expect(tree.readContent('/domain-0002s/domain-0002s-by-domain-0001-id.loader.ts'))
         .toEqual(`import { Injectable, Scope } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import DataLoader from 'dataloader';
 import { Maybe } from 'graphql/jsutils/Maybe';
-import { In, Repository } from 'typeorm';
+import { In } from 'typeorm';
 
-import { Domain0001 } from './loader-0001.entity';
+import { Domain0010 } from './domain-0010.entity';
+import { Domain0010Repository } from './domain-0010.repository';
 
 @Injectable({ scope: Scope.REQUEST })
-export class Domain0001ByIdLoader extends DataLoader<
+export class Domain0010sByDomain0009IdLoader extends DataLoader<
   string,
-  Maybe<Domain0001>
+  Maybe<Domain0010[]>
 > {
-  constructor(
-    @InjectRepository(Domain0001)
-    private readonly repo: Repository<Domain0001>,
-  ) {
-    super(async (keys: readonly string[]): Promise<Maybe<Domain0001>[]> => {
+  constructor(private readonly repo: Domain0010Repository) {
+    super(async (keys: readonly string[]): Promise<Maybe<Domain0010[]>[]> => {
       const daoArray = await this.repo.find({
         where: {
-          id: In(keys),
+          domain0009Id: In(keys),
         },
       });
-      const daoMap = new Map(daoArray.map((dao) => [dao.id, dao]));
-      return keys.map((key) => daoMap.get(key));
+      return keys.map((key) =>
+        daoArray.filter((dao) => key === dao.domain0009Id),
+      );
     });
   }
 }
@@ -72,32 +70,31 @@ export class Domain0001ByIdLoader extends DataLoader<
     });
 
     it('should generate "Domain0001ByIdResolver" class', () => {
-      expect(tree.readContent('/loader-0001/loader-0001-by-id.resolver.ts'))
+      expect(tree.readContent('/domain-0002s/domain-0002s-by-domain-0001-id.resolver.ts'))
         .toEqual(`import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { Maybe } from 'graphql/jsutils/Maybe';
 
-import { Domain0001ByIdLoader } from './loader-0001-by-id.loader';
-import { Domain0001 } from './loader-0001.entity';
-import { Domain0001ById } from './query/loader-0001-by-id.type';
+import { Domain0010 } from './domain-0010.entity';
+import { Domain0010sByDomain0009IdLoader } from './domain-0010s-by-domain-0009-id.loader';
+import { Domain0010sByDomain0009Id } from './query/domain-0010s-by-domain-0009-id.type';
 
-@Resolver(() => Domain0001ById)
-export class Domain0001ByIdResolver {
-  constructor(private readonly loader: Domain0001ByIdLoader) {}
+@Resolver(() => Domain0010sByDomain0009Id)
+export class Domain0010sByDomain0009IdResolver {
+  constructor(private readonly loader: Domain0010sByDomain0009IdLoader) {}
 
-  @ResolveField(() => Domain0001, { nullable: true })
-  async domain0001(
-    @Parent() { domain0001Id, domain0001 }: Domain0001ById,
-  ): Promise<Maybe<Domain0001>> {
-    if (domain0001) return domain0001;
-    if (domain0001Id) return this.loader.load(domain0001Id);
-    return null;
+  @ResolveField(() => [Domain0010], { nullable: true })
+  async domain0010s(
+    @Parent() { id, domain0010s }: Domain0010sByDomain0009Id,
+  ): Promise<Maybe<Domain0010[]>> {
+    if (domain0010s) return domain0010s;
+    return this.loader.load(id);
   }
 }
 `);
     });
 
     it('should generate "Domain0001ById" class', () => {
-      expect(tree.readContent('/loader-0001/query/loader-0001-by-id.type.ts'))
+      expect(tree.readContent('/domain-0002s/query/domain-0002s-by-domain-0001-id.type.ts'))
         .toEqual(`import { Field, ID, InterfaceType } from '@nestjs/graphql';
 import { Maybe } from 'graphql/jsutils/Maybe';
 
